69ce7aaf0981eb1108cd91a36ac5d005764b874a
abyalax <abyalaxx@gmail.com>
2024-10-29 19:30:20 +0700

    Test Commit from different Environment

--- src/utils/groq.js
+++ src/utils/groq.js
@@ -1,14 +1,14 @@
-import {Groq} from "groq-sdk"
+import { Groq } from "groq-sdk"
 const GROQ_API = import.meta.env.VITE_GROQ;
 
 const groq = new Groq({
-  apiKey: GROQ_API,
-  dangerouslyAllowBrowser: true
+    apiKey: GROQ_API,
+    dangerouslyAllowBrowser: true
 });
 
-export const requestToGroqAI = async(content) => {
+export const requestToGroqAI = async (content) => {
     const reply = await groq.chat.completions.create({
-        messages:[{
+        messages: [{
             "role": "user",
             content,
         }], model: "llama3-8b-8192"
